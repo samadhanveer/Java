19fb0cda8212380bbbc012cda3cd782d748d02cf
samadhan <samadhan@gmail.com>
2022-08-31 11:07:07 +0530

    First Commit

--- first.java
+++ first.java
@@ -3,5 +3,10 @@ public class first{
     public static void main (String args[]){
 
         System.out.println("hello");
+        System.out.println("one");
+        System.out.println("two");
+        System.out.println("three");
+
+
     }
 }
