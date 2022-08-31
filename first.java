d01aea944376de32982c7aa57c3920fe3843e4da
samadhan <samadhan@gmail.com>
2022-08-31 12:26:44 +0530

    sop deleted

--- first.java
+++ first.java
@@ -4,14 +4,7 @@ public class first{
 
        
         System.out.println("hello");
-        System.out.println("one");
-        System.out.println("two");
-        System.out.println("three");
-        System.out.println("four");
-        System.out.println("five");
-        System.out.println("six");
-        System.out.println("seven");
-
+        
 
     }
     public void add(){
